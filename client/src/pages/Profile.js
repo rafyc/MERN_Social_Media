8ba2cd3b048a65fc4bdf80eb5c561fdfa0753b1c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2022-09-29 13:57:54 +0200

    first front push befor change react version

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Log from '../components/Log';
 
 const Profile = () => {
   return (
-    <div>
-      Le Profile
+    <div className='profil-page'>
+      <div className='log-container'>
+        <Log signin={false} signup />
+        <div className='img-container'>
+          <img src='./img/log.svg' alt='img-log' />
+        </div>
+      </div>
     </div>
   );
 };
