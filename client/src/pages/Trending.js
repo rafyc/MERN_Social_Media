6df2aa294a4b6e60f91c0b3abece6f1ec7b4828a
Come Le Sauter <come.l.s@hotmail.fr>
2022-10-24 11:16:20 +0200

    key error

--- client/src/pages/Trending.js
+++ client/src/pages/Trending.js
@@ -1,23 +1,22 @@
-import React, { useContext } from 'react';
-import { useSelector } from 'react-redux';
-import { UidContext } from '../components/AppContext';
-import LeftNav from '../components/LeftNav';
-import Card from '../components/Post/Card';
-import Trends from '../components/Trends';
-import FriendHint from '../components/Profile/FriendHint';
-import { isEmpty } from '../components/Utils';
-
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import { UidContext } from "../components/AppContext";
+import LeftNav from "../components/LeftNav";
+import Card from "../components/Post/Card";
+import Trends from "../components/Trends";
+import FriendHint from "../components/Profile/FriendHint";
+import { isEmpty } from "../components/Utils";
 
 const Trending = () => {
-  const uid = useContext(UidContext)
+  const uid = useContext(UidContext);
   const trendList = useSelector((state) => state.trendingReducer);
   return (
     <div className="trending-page">
       <LeftNav />
       <div className="mai">
         <ul>
-          {!isEmpty(trendList[0]) && trendList.map((post) => <Card post=
-            {post} key={post.id} />)}
+          {!isEmpty(trendList[0]) &&
+            trendList.map((post) => <Card post={post} key={post._id} />)}
         </ul>
       </div>
       <div className="right-side">
